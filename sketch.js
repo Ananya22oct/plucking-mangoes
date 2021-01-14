1bd848a2dd19ba0ec03042ce64d1a635e7ea0e1e
Ananya22oct <75477416+Ananya22oct@users.noreply.github.com>
2021-01-14 14:15:02 +0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,102 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint=Matter.Constraint;
+
+ var mango;
+ var stone;
+ var ground;
+ var tree;
+ var boy;
+ var chain;
+
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(1000, 600);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+    mango1 = new Mango(920,300,30);
+	mango2 = new Mango(820,250,30);
+	mango3 = new Mango(800,200,30);
+	mango4 = new Mango(890,300,30);
+	mango5 = new Mango(800,300,30);
+	mango6 = new Mango(800,300,30);
+  mango7 = new Mango(820,280,30);
+    stone=new Stone(500,400,20);
+    ground=new Ground(500,590,1000,20);
+    tree=new Tree(810,400);
+    
+  boy=new Boy(220,510,250,250)
+    chain = new Chain(stone.body,{x:140, y:450});
+
+
+	Engine.run(engine);
+  
+}
+
+function draw() {
+  rectMode(CENTER);
+  background("pink");
+
+
+  tree.display();
+ mango1.display();
+ mango2.display();
+ mango3.display();
+ mango4.display();
+ mango5.display();
+ mango6.display();
+ mango7.display();
+ 
+  stone.display();
+  ground.display();
+ 
+  boy.display();
+  chain.display();
+
+  detectCollision(stone,mango1);
+  detectCollision(stone,mango2);
+  detectCollision(stone,mango3);
+  detectCollision(stone,mango4);
+  detectCollision(stone,mango5);
+  detectCollision(stone,mango6);
+  detectCollision(stone,mango7);
+  
+  
+  drawSprites();
+ 
+}
+function mouseDragged(){
+  Matter.Body.setPosition(stone.body,{x:mouseX, y:mouseY});
+}
+function mouseReleased(){
+  chain.fly();
+}
+function keyPressed(){
+if(keyCode === 32){
+  Matter.Body.setPosition(stone.body,{x:140, y:450});
+  chain.attach(stone.body);
+}
+}
+function detectCollision(lstone,lmango){
+mangoBodyPosition=lmango.body.position
+stoneBodyPosition=lstone.body.position
+
+var distance=dist(stoneBodyPosition.x,stoneBodyPosition.y,mangoBodyPosition.x,mangoBodyPosition.y)
+if(distance<=lmango.r+lstone.r){
+  Matter.Body.setStatic(lmango.body,false);
+}
+}
+ 
+
